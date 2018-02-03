bed88812eb7bd600d0545cca83083992b0806e3e
João Alexandre <joao.a@clinicasim.local>
2018-02-03 14:24:26 -0300

    vai

---
+++ se05ep07-client/src/main.js
@@ -0,0 +1,26 @@
+// // main.js
+// const foo = require("./mymodule1")
+// const bar = require("./anothermodule").bar
+
+// foo(2)
+// console.log(bar(3))
+
+// alert("hello from browserify")// alert só é válido no browser
+// main.js
+// main.js
+require("./main.css")
+
+const Vue = require("vue")
+const VueRouter = require("vue-router")
+const VueMaterial = require("vue-material")
+
+Vue.use(VueRouter)
+Vue.use(VueMaterial)
+
+new Vue({
+  el: "#mountpoint",
+  render: r => r(require("./spa.vue"))
+})
+
+
+
